26ea11a1dd1ed59f7d99148b00ec6ea27bd926b4
sinxd7 hoxha <denisloris4@gmail.com>
2024-11-08 10:45:59 +0000

    primo commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template,request
+app=Flask(__name__)
+#server web-codice per restituire la homepage 
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+#server API
+@app.route("/calcola", methods=["POST"])
+def calcola():
+    dati = request.get_json() 
+
+
+
+if __name__== "__main__":
+    app.run(host="0.0.0.0",port=3245,debug=True)
