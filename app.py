7f1ff60a015fec6f09bb3170fa53162d4ef6e55c
Andrea Rutigliano <rutigliano.andrea@iisgalvanimi.edu.it>
2024-11-12 11:49:29 +0000

    Commit POST

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request, jsonify
 app=Flask(__name__)
 #server web-codice per restituire la homepage 
 @app.route("/")
@@ -8,9 +8,24 @@ def homepage():
 #server API
 @app.route("/calcola", methods=["POST"])
 def calcola():
-    dati = request.get_json() 
-
-
-
+    #prendere le informazioni in arrivo dal front end
+    json_data = request.get_json() 
+    if json_data:
+        num1 = json_data.get('num1')
+        num2 = json_data.get('num2')
+        operazione = json_data.get('operazione')
+        #elaborazione dell'informazione
+        if operazione == 'addizione':
+            risultato = num1 + num2
+        elif operazione == 'sottrazione':
+            risultato = num1 - num2
+        elif operazione == 'moltiplicazione':
+            risultato = num1 * num2
+        elif operazione == 'divisione':
+            risultato = num1 // num2
+            #restituire i risultati al front end
+        return jsonify(risultato = risultato)
+    else:
+        return jsonify(risultato = 'Mancano i dati')
 if __name__== "__main__":
     app.run(host="0.0.0.0",port=3245,debug=True)
